b87f692241edd9e35cc4e7afb921d4078a1b2848
muhammad ichsan <155188932+iichsan@users.noreply.github.com>
2024-07-26 17:07:46 +0700

    proyekDPSI

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const sequelize = require('../config/database'); // Pastikan path sesuai dengan lokasi file konfigurasi Sequelize
+
+// Di sini Anda bisa mengatur model dan relasi jika diperlukan
+
+module.exports = sequelize;
